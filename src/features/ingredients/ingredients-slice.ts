190dbb758122e78323306ca94e4a341b9cb5ee49
Arseniy Rubtsov <arseniy.rubtsov@braincreators.com>
2022-06-21 21:46:53 +0300

    add redux store and simple ingredients slice

---
+++ src/features/ingredients/ingredients-slice.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+
+export enum IngredientType {
+    Vegetables = 'Vegetables',
+    Fruits = 'Fruits',
+    Meat = 'Meat',
+    Spices = 'Spices',
+    Greens = 'Greens',
+    Herbs = 'Herbs',
+    MilkProducts = 'Milk Products',
+    Eggs = 'Eggs',
+    Fats = 'Fats',
+    BakingProducts = 'Baking Products',
+    Nuts = 'Nuts',
+    Pasta = 'Pasta',
+    Grains = 'Grains',
+    Other = 'Other',
+}
+
+export enum PortionType {
+    Piece = 'piece',
+    Gram = 'gram',
+    TableSpoon = 'table spoon',
+    TeaSpoon = 'tea spoon',
+    Cup = 'cup',
+}
+
+export interface Ingredient {
+    id: string
+    name: string
+    description: string
+    type: IngredientType
+}
+
+interface IngredientsState {
+    ingredients: Ingredient[]
+}
+
+// Define the initial state using that type
+const initialState: IngredientsState = {
+    ingredients: [],
+}
+
+export const ingredientsSlice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {
+    addIngredient(state, action: PayloadAction<Ingredient>) {
+      state.ingredients.push(action.payload)
+    },
+    deleteIngredient(state, action: PayloadAction<string>) {
+        state.ingredients = state.ingredients.filter(ingredient => ingredient.id !== action.payload)
+    }
+  },
+})
+
+export const { addIngredient, deleteIngredient } = ingredientsSlice.actions
+
+export const selectIngredients = (state: RootState) => state.ingredients.ingredients
+
+export default ingredientsSlice.reducer
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
