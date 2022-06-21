c9c0f994a9cc7c740f75039807d5a8f3a5466f2d
Arseniy Rubtsov <arseniy.rubtsov@braincreators.com>
2022-06-21 22:27:50 +0300

    wrap app in BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
