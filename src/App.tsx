48c1cebb61e772a0e87a2418d3c3b8044da1e42c
Arseniy Rubtsov <arseniy.rubtsov@braincreators.com>
2022-06-23 00:51:44 +0300

    fix redirect to ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate,  } from "react-router-dom";
 import { MainLayout, Ingredients } from './components/';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Navigate to="/ingredients" />} />
         <Route path="/ingredients" element={<Ingredients />} />
-        <Route path="*" element={<Navigate to="/ingredients" replace />} />
       </Route>
     </Routes>
   );
